c1d0657848aa82782baa51e1c2c38800927073bd
streimers <streimers@web.de>
2021-03-10 15:45:26 +0100

    Initial Commit

---
+++ MemeMe 1.0 STR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0 STR
+//
+//  Created by Reimers, Tobias Soeren (059) on 10.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
